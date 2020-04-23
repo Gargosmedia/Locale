f05f29558523f75f70962397450212b9e7e4247b
Bogdan <bogdanivanenko@gmail.com>
2020-04-23 13:14:28 -0400

    Changing consumers to make websocket asynchronous

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -82,7 +82,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],  
+            "hosts": [('0.tcp.ngrok.io', 16935)],  
         },
     },
 }
